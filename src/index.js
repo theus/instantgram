36de2bed577569a573ed8ada1fc2d84e8604bb8d
Matheus Falcão <theusx@gmail.com>
2016-08-04 16:51:05 -0300

    Video support is on the boat!

--- src/index.js
+++ src/index.js
@@ -21,49 +21,77 @@ const regex_image_id = /pImage_\d+/
 =            Program            =
 ===============================*/
 if ( regex_hostname.test(hostname) ) {
-  if ( regex_path.test(path) ) { // verify if are in instagram post
 
-    /*=======================================
-    =            Instagram Modal            =
-    =======================================*/
-    try {
-      if (document.getElementsByTagName('article').length === 2) { // when instagram post is a modal
-        let modal = document.getElementsByTagName('article')[1]
-        let modal_image = modal.querySelector("header + div img").src
-
-        if (modal_image) {// verify if finds a post image
-          { // bring the original image if had
-          var image_link = (regex_original_image.test(modal_image)) ? modal_image.replace(regex_original_image, '') : modal_image
+  var foundVideo = false
+  /*===============================================
+  =            Video visible in screen            =
+  ===============================================*/
+  try {
+    search_video: {
+      for (let video of document.querySelectorAll('video')) {
+        if ( true ) { // no verification
+          if ( isElementInViewport(video) ) { // verify if is in viewport
+            let video_link = video.src
+            if (video_link) {
+              // open image in new tab
+              window.open(video_link)
+              foundVideo = true
+              alertNotInInstagramPost = true // if don't find nothing, alert to open the post
+            }
+            break search_video // if found the image stop searching
           }
-          // open image in new tab
-          window.open(image_link)
-        }else{
-          alert("[instantgram] don't found a image in instagram post. Try open the link in new tab.")
-        }
-
-      }
-    } catch(e) { console.error(`[instantgram] ${VERSION}`, e)}
-    /*=====  End of Instagram Modal  ======*/
-
-    /*======================================
-    =            Instagram Post            =
-    ======================================*/
-    try {
-      if (document.getElementsByTagName('article').length === 1) { // verify if has a image post
-        let post = document.getElementsByTagName('article')[0]
-        let post_image = document.querySelector("article > div img").src
-        { // bring the original image if had
-          var image_link = (regex_original_image.test(post_image)) ? post_image.replace(regex_original_image, '') : post_image
-        }
-        if (image_link) {
-          // open image in new tab
-          window.open(image_link)
-        }else{
-          alert ("ops, [instantgram] don't found the image :(")
         }
       }
-    } catch(e) {console.error(`[instantgram] ${VERSION}`, e)}
-    /*=====  End of Instagram Post  ======*/
+    }
+  } catch(e) { console.error(`[instantgram] ${VERSION}`, e) }
+  /*=====  End of Video visible in screen  ======*/
+
+
+  if ( regex_path.test(path) ) { // verify if are in instagram post
+    if (!foundVideo) { // if not find a video, search images
+
+      /*=======================================
+      =            Instagram Modal            =
+      =======================================*/
+      try {
+        if (document.getElementsByTagName('article').length === 2) { // when instagram post is a modal
+          let modal = document.getElementsByTagName('article')[1]
+          let modal_image = modal.querySelector("header + div img").src
+
+          if (modal_image) {// verify if finds a post image
+            { // bring the original image if had
+            var image_link = (regex_original_image.test(modal_image)) ? modal_image.replace(regex_original_image, '') : modal_image
+            }
+            // open image in new tab
+            window.open(image_link)
+          }else{
+            alert("[instantgram] don't found a image in instagram post. Try open the link in new tab.")
+          }
+
+        }
+      } catch(e) { console.error(`[instantgram] ${VERSION}`, e)}
+      /*=====  End of Instagram Modal  ======*/
+
+      /*======================================
+      =            Instagram Post            =
+      ======================================*/
+      try {
+        if (document.getElementsByTagName('article').length === 1) { // verify if has a image post
+          let post = document.getElementsByTagName('article')[0]
+          let post_image = document.querySelector("article > div img").src
+          { // bring the original image if had
+            var image_link = (regex_original_image.test(post_image)) ? post_image.replace(regex_original_image, '') : post_image
+          }
+          if (image_link) {
+            // open image in new tab
+            window.open(image_link)
+          }else{
+            alert ("ops, [instantgram] don't found the image :(")
+          }
+        }
+      } catch(e) {console.error(`[instantgram] ${VERSION}`, e)}
+      /*=====  End of Instagram Post  ======*/
+    }
   } else {
     var alertNotInInstagramPost = true
   }
@@ -72,7 +100,7 @@ if ( regex_hostname.test(hostname) ) {
   =            Image visible in screen            =
   ===============================================*/
   try {
-    if (!image_link) { // verify if already found a image
+    if (!image_link && !foundVideo) { // verify if already found a image
       search: {
         for (let image of document.images) {
           if ( regex_image_id.test(image.id) ) { // get only images posts
@@ -97,7 +125,7 @@ if ( regex_hostname.test(hostname) ) {
   } catch(e) { console.error(`[instantgram] ${VERSION}`, e) }
   /*=====  End of Image visible in screen  ======*/
 
-  if (alertNotInInstagramPost) alert ("ops, are you in a instagram post? ex: instagram.com/p/82jd828jd")
+  if (alertNotInInstagramPost && !foundVideo) alert ("ops, are you in a instagram post? ex: instagram.com/p/82jd828jd")
   update(VERSION)
 }
 /*=====  End of Program  ======*/
