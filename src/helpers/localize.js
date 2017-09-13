2e9b57e8556b0488c25084da5d99cb7954cb4ad5
Matheus Falcão <theusx@gmail.com>
2017-09-12 22:32:40 -0300

    :ok_hand: fix grammar and bump version

--- src/helpers/localize.js
+++ src/helpers/localize.js
@@ -25,7 +25,7 @@ function localize (str, lang = LANG_DEFAULT) {
     }
   } catch (e) {
     console.error('[instantgram]LOC error:', e)
-    return `ops, a error ocurred in localization system. Enter in https://github.com/theus/instantgram/issues/new and open a issue with this code: 'LOC_dont_found_str_neither_default:[${lang}->${str}]'
+    return `ops, an error ocurred in localization system. Enter in https://github.com/theus/instantgram/issues/new and open an issue with this code: 'LOC_dont_found_str_neither_default:[${lang}->${str}]'
     for more information open the console`
   }
 }
