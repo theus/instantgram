5d916e410921281e31d7ef29854215907de9fccd
ThinkBIG-Company <info@think-big-company.com>
2020-07-30 21:13:33 +0200

    Spanish (Argentina) has been added.
    Spanish (Argentina) has been added.

--- src/localization.js
+++ src/localization.js
@@ -1,5 +1,20 @@
 export default {
   'langs': {
+	'de-DE': {
+      'helpers.localize_defaultlang': 'Ausgewählte Sprache: ${LANG_DEFAULT} \n Weitere Informationen zu den unterstützten Sprachen findest du auf http://theus.github.io/instantgram',
+
+      'modules.update@oudated_outdated': '[instantgram] ist veraltet. Bitte besuche die Seite http://theus.github.io/instantgram für ein Update.',
+      'modules.update@oudated_localInfo': '[instantgram] Installierte Version ${data.version} | Neue Version: ${data.gitVersion}',
+      'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] sucht nach neuen verfügbaren Updates…',
+      'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] wurde aktualisiert.',
+      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] hat ein neues Update gefunden.\nBitte besuche die Seite http://theus.github.io/instantgram, um das Update zu installieren.',
+
+      'index@alert_onlyWorks': '[instantgram] funktioniert nur mit instagram.com.',
+      'index#program#modal@alert_dontFound': "[instantgram] konnte kein Bild in diesem Post finden. Bitte öffne den Link in einem neuen Tab.",
+      'index#program#post@alert_dontFound': "Ops, [instantgram] konnte leider kein Bild finden  :-(",
+      'index#program#screen@alert_dontFound': '[instantgram] hat mehr als 1 Bild gefunden. Bist du in der Profilansicht? Falls ja, öffne bitte zuerst einen einzelnen Post und führe [instantgram] erneut aus.',
+      'index#program@alert_dontFound': 'Ops, hast du einen Instagram Post geöffnet? Zum Beispiel instagram.com/p/82jd828jd'
+    },  
     'en-US': {
       'helpers.localize_defaultlang': '[instantgram] set language: ${LANG_DEFAULT} \n For more information about available languages please check http://theus.github.io/instantgram',
 
@@ -7,15 +22,29 @@ export default {
       'modules.update@oudated_localInfo': '[instantgram] Installed version: ${data.version} | New update: ${data.gitVersion}',
       'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] is looking for available updates…',
       'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] updated your current version.',
-      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] found a new available update.\nPlease check <a href="http://theus.github.io/instantgram">http://theus.github.io/instantgram</a> to install it.',
+      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] found a new available update.\nPlease check http://theus.github.io/instantgram to install it.',
 
       'index@alert_onlyWorks': '[instantgram] only works on instagram.com.',
       'index#program#modal@alert_dontFound': "[instantgram] didn't find any image in this Instagram post. Please try to open the link in a new tab.",
       'index#program#post@alert_dontFound': "Ops, [instantgram] couldn't find any image  :-(",
       'index#program#screen@alert_dontFound': '[instantgram] found more than 1 image. Are you on a profile page? If yes, please open a single post first and open [instantgram] again.',
-      'index#program@alert_dontFound': 'Ops, did you open any Instagram post? Like for example instagram.com/p/82jd828jd',
-      'index#program@alert_videoBlob': "If you're trying open a video, try enter on Instagram post page. Like for example instagram.com/p/82jd828jd. If you're already in it, try refreshing the page before running [instantgram] or open the post in a new tab. (this proccess is neccessary only for videos)"
+      'index#program@alert_dontFound': 'Ops, did you open any Instagram post? Like for example instagram.com/p/82jd828jd'
     },
+	'es-AR': {
+      'helpers.localize_defaultlang': '[instantgram] elegir idioma: ${LANG_DEFAULT} \n Para más información acerca de los idiomas disponibles, por favor visite http://theus.github.io/instantgram',
+
+      'modules.update@oudated_outdated': '[instantgram] está desactualizado. Por favor visite http://theus.github.io/instantgram para ver actualizaciones.',
+      'modules.update@oudated_localInfo': '[instantgram] Versión instalada: ${data.version} | Nueva actualización: ${data.gitVersion}',
+      'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] está buscando nuevas actualizaciones…',
+      'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] actualizó a la versión actual.',
+      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] encontró una nueva actualización disponible.\nPor favor visite http://theus.github.io/instantgram para instalarla.',
+
+      'index@alert_onlyWorks': '[instantgram] sólo funciona en instagram.com.',
+      'index#program#modal@alert_dontFound': "[instantgram] no encontró ninguna imagen en esta publicación de Instagram. Por favor intente abrir el link en una nueva pestaña.",
+      'index#program#post@alert_dontFound': "Ups, [instantgram] no pudo encontrar ninguna imagen :-(",
+      'index#program#screen@alert_dontFound': '[instantgram] encontró más de 1 imagen. ¿Estás en una página de perfil? Si es así, por favor ingresa en una publicación y luego abre [instantgram] nuevamente.',
+      'index#program@alert_dontFound': 'Ups, abriste alguna publicación de Instagram? Por ejemplo instagram.com/p/82jd828jd'
+    }, 
     'pt-BR': {
       'helpers.localize_defaultlang': '[instantgram] idioma configurado: ${LANG_DEFAULT} \npara mais informações sobre os idiomas suportados, acesse http://theus.github.io/instantgram',
 
@@ -29,24 +58,7 @@ export default {
       'index#program#modal@alert_dontFound': '[instantgram] não encontrou uma imagem em um post. Tente abrir o link em uma nova aba.',
       'index#program#post@alert_dontFound': 'ops, [instantgram] não encontrou a imagem :(',
       'index#program#screen@alert_dontFound': '[instantgram] a procura por imagem na tela encontrou mais de 1 imagem. Você está em um perfil? Se sim, abra alguma imagem antes de rodar o [instantgram]',
-      'index#program@alert_dontFound': 'ops, você está em algum post do instagram? ex: instagram.com/p/82jd828jd',
-      'index#program@alert_videoBlob': "Se você está tentando abrir um vídeo, tente entrar na página do post do Instagram. ex: instagram.com/p/82jd828jd. Se você já está na página, tente atualizar a página antes de executar o [instantgram] ou abra o post em uma nova aba. (este processo só é necessário com vídeos)"
-    },
-    'de-DE': {
-      'helpers.localize_defaultlang': 'Ausgewählte Sprache: ${LANG_DEFAULT} \n Weitere Informationen zu den unterstützten Sprachen findest du auf http://theus.github.io/instantgram',
-
-      'modules.update@oudated_outdated': '[instantgram] ist veraltet. Bitte besuche die Seite http://theus.github.io/instantgram für ein Update.',
-      'modules.update@oudated_localInfo': '[instantgram] Installierte Version ${data.version} | Neue Version: ${data.gitVersion}',
-      'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] sucht nach neuen verfügbaren Updates…',
-      'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] wurde aktualisiert.',
-      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] hat ein neues Update gefunden.\nBitte besuche die Seite http://theus.github.io/instantgram, um das Update zu installieren.',
-
-      'index@alert_onlyWorks': '[instantgram] funktioniert nur mit instagram.com.',
-      'index#program#modal@alert_dontFound': "[instantgram] konnte kein Bild in diesem Post finden. Bitte öffne den Link in einem neuen Tab.",
-      'index#program#post@alert_dontFound': "Ops, [instantgram] konnte leider kein Bild finden  :-(",
-      'index#program#screen@alert_dontFound': '[instantgram] hat mehr als 1 Bild gefunden. Bist du in der Profilansicht? Falls ja, öffne bitte zuerst einen einzelnen Post und führe [instantgram] erneut aus.',
-      'index#program@alert_dontFound': 'Ops, hast du einen Instagram Post geöffnet? Zum Beispiel instagram.com/p/82jd828jd',
-      'index#program@alert_videoBlob': "If you're trying open a video, try enter on Instagram post page. Like for example instagram.com/p/82jd828jd. If you're already in it, try refreshing the page before running [instantgram] or open the post in a new tab. (this proccess is neccessary only for videos) // we need translate this string to Deutsch, if you can help us enter in contact by Github"
+      'index#program@alert_dontFound': 'ops, você está em algum post do instagram? ex: instagram.com/p/82jd828jd'
     },
   }
 }
